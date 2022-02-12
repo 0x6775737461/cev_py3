84e899884a2ad4e7e282f0ddfe47f0b06f981ca8
0x6775737461 <fabiogustavo@tuta.io>
2022-02-12 15:22:18 -0400

    resolvendo o exercício001

---
+++ ex001/hello_world.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+'''
+resolvendo o exercicío 1
+do curso em vídeo
+'''
+
+def main():
+    '''
+    usando o método print()
+    para mostrar 'Olá, Mundo'
+    '''
+
+    print('Olá, Mundo!')
+
+if __name__ == "__main__":
+    main()
