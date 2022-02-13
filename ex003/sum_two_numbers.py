2337f5d2bbb49040861bfc271c104bb56ab2f3d0
0x6775737461 <fabiogustavo@tuta.io>
2022-02-13 13:29:36 -0400

    solução do 3º exercício

---
+++ ex003/sum_two_numbers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+def main():
+    a = int(input('Digite um número: '))
+    b = int(input('Digite outro número: '))
+
+    print(f"A soma entre {a} e {b} é igual a {a+b}")
+
+if __name__ == "__main__":
+    main()
