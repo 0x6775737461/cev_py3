f3831303634489e2f13ab35382d3093727fd1ad2
0x6775737461 <fabiogustavo@tuta.io>
2022-02-13 13:10:31 -0400

    solução do 2º exercício

---
+++ ex002/type_ur_name.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+def main():
+    name = input("Digite o seu nome: ")
+
+    print(f"É um prazer te conhecer, {name}!")
+
+if __name__ == "__main__":
+    main()
